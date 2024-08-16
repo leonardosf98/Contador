2ade10525227a0d720cd15f0109b71c7eaf3d739
Leonardo Souza <leonardo-f98@hotmail.com>
2024-08-16 20:13:24 -0300

    feat: creates main method and personalized exception

---
+++ src/Contador.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int firstParameter = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int secondParameter = terminal.nextInt();
+
+        try {
+            count(firstParameter, secondParameter);
+        }catch (ParametrosInvalidosException exception) {
+                exception.printStackTrace();
+            }
+    }
+
+    static void count(int firstParameter, int secondParameter ) throws ParametrosInvalidosException {
+        if(secondParameter < firstParameter){
+            throw new ParametrosInvalidosException("O primeiro parâmetro precisa ser menor que o segundo");
+    }
+        int count = secondParameter - firstParameter;
+        for(int i = 1; i <= count; i ++  ){
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
